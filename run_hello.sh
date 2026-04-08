b96aa74695c5bb6c0f0e63f4b255f0febb2b76ed
Manne Hannula <manneha@oamk.fi>
2026-04-08 17:47:02 +0300

    update 2

--- run_hello.sh
+++ run_hello.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+#SBATCH --account=project_2018026
+#SBATCH --job-name=my_own
+#SBATCH --output=log_file.txt
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --partition=test
+
+module load python-data
+
 python hello.py
+
+
